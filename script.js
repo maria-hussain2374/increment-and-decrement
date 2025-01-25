aef8094717d2f7d836199a40ac97717cb8ee8353
Maria <amnaattaria325@gmail.com>
2025-01-25 12:44:29 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let count=0;
+function increase(){
+    count++;
+    document.getElementById("count").innerHTML=count;
+}
+function decrease(){
+    count--;
+    document.getElementById("count").innerHTML=count;
+}
+function reset(){
+    count=0;
+    document.getElementById("count").innerHTML=count;
+}
